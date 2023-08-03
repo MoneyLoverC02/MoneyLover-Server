204b6893da465b6a44a46f46dab48614aba51ab8
HieuT23511 <hieut23511@gmail.com>
2023-08-03 11:16:37 +0700

    api getlistUser

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from "express";
 import {AppDataSource} from "../models/data-source";
 import {User} from "../models/entity/User";
+const userRepository = AppDataSource.getRepository(User);
 
 class userController {
     static async createUser(req: Request, res: Response) {
         try {
-            const userRepository = AppDataSource.getRepository(User);
             const {email, password} = req.body;
             let user = await userRepository.findOneBy({email: email});
             if (!user) {
@@ -30,9 +30,22 @@ class userController {
             })
         }
     }
-
-
+static async getListUser(req: Request, res: Response){
+    try {
+            const users = await userRepository.find()
+        if (users){
+            res.status(200).json({
+                message: "Get list users successfully",
+                listUser: users
+            })
+        }
+        } catch (err){
+            res.status(500).json({
+                message: err.message
+            })
+        }
 }
 
+}
 export default userController;
 
--- src/routers/User.router.ts
+++ src/routers/User.router.ts
@@ -4,5 +4,6 @@ import userController from "../controllers/User.controller";
 const userRouter = express.Router();
 
 userRouter.post('/users',userController.createUser);
+userRouter.get('/users',userController.getListUser);
 
 export default userRouter;
