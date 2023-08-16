f15c1b622470d67a0a60e260f0d3150bef444b0a
HieuT23511 <hieut23511@gmail.com>
2023-08-16 08:22:08 +0700

    reset password

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -8,6 +8,6 @@ authRouter.post('/register',userController.createUser);
 authRouter.post('/login', userController.login);
 // authRouter.post('/sendConfirmEmail', UserController.sendEmail)
 authRouter.get('/verify/:token', UserController.verifyEmail)
-
+authRouter.get('/reset-password',UserController.resetPassword)
 
 export default authRouter;
