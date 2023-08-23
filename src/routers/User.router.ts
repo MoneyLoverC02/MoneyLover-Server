2380279b34ba967896871fab1a4fb4f80949bfeb
HieuT23511 <hieut23511@gmail.com>
2023-08-23 12:09:20 +0700

    cm

--- src/routers/User.router.ts
+++ src/routers/User.router.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import UserController from "../controllers/User.controller";
-import userController from "../controllers/User.controller";
 
 const userRouter = express.Router();
 
 userRouter.get('/users', UserController.getListUser);
 userRouter.delete('/users', UserController.deleteUser);
 userRouter.put('/users', UserController.updateUser);
-userRouter.get('/users/:userID/send-report', userController.sendReport)
+userRouter.get('/users/:userID/send-report', UserController.sendReport)
 export default userRouter;
